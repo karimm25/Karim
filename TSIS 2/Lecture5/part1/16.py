5584db47c56b249c4dae65d7180d9c777483483b
Karim <karimseitkarimov@gmail.com>
2021-07-08 12:31:56 +0600

    TSIS 2

---
+++ TSIS 2/Lecture5/part1/16.py
@@ -0,0 +1,8 @@
+
+ # Write a Python program to assess if a file is closed or not.
+
+f = open("qwerty.txt")
+
+print(f.closed)
+f.close()
+print(f.closed)
---
+++ TSIS 2/Lecture5/part1/9.py
@@ -0,0 +1,9 @@
+
+# Write a Python program to count the number of lines in a text file
+f = open("qwerty.txt")
+
+c = 0
+for i in f.read().split("\n"):
+    if i:
+        c = c + 1
+print(c)
